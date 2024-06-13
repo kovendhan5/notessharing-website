8035ee59b88c9a9ad67215d052bfc079b8447b22
kovendhan5 <kovendhanofficial5@gmail.com>
2024-06-13 14:57:40 +0530

    new update

--- client/main.js
+++
@@ -1,6 +0,0 @@
-// YOUR JAVASCRIPT CODE FOR INDEX.HTML GOES HERE
-const { google } = require('googleapis');
-const CLIENT_ID = '130258146268-sl8ogrj61jqe2legdtfvgm4m0ud7chdb.apps.googleusercontent.com';
-CLIENT_SECRET = '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';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
- const REFRESH_TOKEN  ='1//047Lp15iw79QCCgYIARAAGAQSNwF-L9IrNldpNI6jyGiQpfq73iDcZPlFn6dQaQy_sXMuhd92sBCtFjL0fCzUWpAOrxs_iyO1KkY';
