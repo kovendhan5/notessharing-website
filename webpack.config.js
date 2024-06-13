79e3b36b55a696943f57d2ef0c5cdc4b549207cd
kovendhan5 <kovendhanofficial5@gmail.com>
2024-06-13 22:03:50 +0530

    creating a webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const path = require('path-browserify');
+const os = require('os-browserify/browser');
+const crypto = require('crypto-browserify');
+
+module.exports = {
+  // ...
+  resolve: {
+    fallback: {
+      path: path,
+      os: os,
+      crypto: crypto
+    }
+  }
+};
+
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'development'
+};
